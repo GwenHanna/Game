e258d635affa5326831c6c7d799f70d706ffc999
gwendoline <gwendoline.garlet+commit@live.fr>
2023-05-15 13:39:29 +0200

    test6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MemoryComponent } from './memory/memory.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MemoryComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
